6e0722f0d03606ea3f89d34c15e32c9dc5d7e57c
DiogoJoseFreitas <diogoJoseFreitas@proton.me>
2025-06-05 15:32:33 -0400

    Added path history to AppSettings

--- Settings/AppSettings.cs
+++ Settings/AppSettings.cs
@@ -3,6 +3,14 @@ namespace TemplateLoader.Settings
     public class AppSettings
     {
         public required string TemplateFolder { get; set; }
+        public List<string> PathsHistory { get; set; } = [];
+
+        public void AddPath(string path)
+        {
+            PathsHistory.Remove(path);
+            PathsHistory.Insert(0, path);
+            PathsHistory = PathsHistory.Take(5).ToList();
+        }
     }
     
 }
